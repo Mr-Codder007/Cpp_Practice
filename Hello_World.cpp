064aa7c78c52f58cbea1e0ee489db38796be8224
Mr-Codder007 <mr.codder007@gmail.com>
2026-06-05 09:54:30 +0530

    Adding new code lines but in a different branch

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 int main()
 {
+    int age;
     std::cout << "Hello World!" << std::endl;
+    std::cout << "What is you age";
+    std::cin >> age;
+    std::cout << "Your age is "<<age<<std::endl;
     return 0;
 }
